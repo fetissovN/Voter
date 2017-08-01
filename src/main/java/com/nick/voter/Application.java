51cc5f09781ba9133bcda08cbf4d9a68c45f4fa9
fetissovN <fetissov.n@gmail.com>
2017-08-01 18:29:48 +0300

    first

---
+++ src/main/java/com/nick/voter/Application.java
@@ -0,0 +1,22 @@
+package com.nick.voter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        return application.sources(Application.class);
+    }
+
+}
